18f70159a862c95063d25e7c3d43aa2a64ee8d48
chenxi <415662468@qq.com>
2019-07-09 10:51:05 +0800

    extract git hub config from code to properties

--- src/main/java/life/december/community/controller/AuthorizeController.java
+++ src/main/java/life/december/community/controller/AuthorizeController.java
@@ -13,20 +13,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
-    @Value("${github.clientId}")
+    @Value("${github.client.id}")
     private String clientId;
     @Value("${github.client.secret}")
     private String clientSecret;
-    @Value("${github..redirect.uri}")
-    private  String clientRedirectUri;
+    @Value("${github.redirect.uri}")
+    private  String redirectUri;
     @GetMapping("/callback")
     public  String callback(@RequestParam(name="code") String code,
                             @RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("clientId");
-        accessTokenDTO.setClient_secret("clientSecret");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("clientRedirectUri");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
 
